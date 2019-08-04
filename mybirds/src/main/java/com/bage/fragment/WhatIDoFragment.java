a9650ba14aa9f39ac51c2b28fd83d7d8a729a023
JinSeJack <wangjianming1994@163.com>
2019-08-04 20:18:09 +0800

    蝉、鸟被选以后使用一个对话框予以展示

--- mybirds/src/main/java/com/bage/fragment/WhatIDoFragment.java
+++ mybirds/src/main/java/com/bage/fragment/WhatIDoFragment.java
@@ -1,22 +1,39 @@
 package com.bage.fragment;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import com.bage.mybirds.R;
+import com.bage.activity.MenuActivity;
 
-public class WhatIDoFragment extends BirdsFragment {
+public class WhatIDoFragment extends DialogFragment {
+
+    public static final String bird = "鸟";
+    public static final String cicada = "蝉";
+    private String[] species = {bird, cicada};
+
+    private WhatIDoFragment.SpeciesChooseListener speciesChooseListener;
 
     @Override
-    View inflateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_birds2, null);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setItems(species, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                if (speciesChooseListener != null)
+                    speciesChooseListener.onSpeciesChoosed(species[which]);
+            }
+        });
+        return builder.create();
     }
 
-    @Override
-    protected void initData(View mCurrentView, Bundle savedInstanceState) {
+    public void setSpeciesChooseListener(WhatIDoFragment.SpeciesChooseListener speciesChooseListener) {
+        this.speciesChooseListener = speciesChooseListener;
+    }
 
+    public interface SpeciesChooseListener {
+        void onSpeciesChoosed(String mode);
     }
 
 }
